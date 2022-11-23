8469f93803b2777e1f092cc95b228c2ede5c044b
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-11-23 16:13:54 +0000

    Basic balloon movement

--- Assets/Scripts/BalloonController.cs
+++ Assets/Scripts/BalloonController.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BalloonController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameManager gameManager;
+    private Rigidbody balloonRb;
+    public float minSpeed;
+    public float maxSpeed;
+    public float xSpawnRange;
+    public float ySpawnPos;
+
     void Start()
     {
-        
+        balloonRb = GetComponent<Rigidbody>();
+        transform.position = RandomSpawnPos();
+        balloonRb.AddForce(RandomForce(), ForceMode.Impulse);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other) 
     {
-        
+        Destroy(gameObject);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xSpawnRange, xSpawnRange), ySpawnPos, 0);
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
     }
 }

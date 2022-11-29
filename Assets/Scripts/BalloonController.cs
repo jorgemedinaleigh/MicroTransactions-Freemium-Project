08e4fab2a7323b40c54f09a442b35fe186ab0e33
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-11-29 20:08:22 +0000

    Added colored materials and particle effect on balloon pop

--- Assets/Scripts/BalloonController.cs
+++ Assets/Scripts/BalloonController.cs
@@ -3,7 +3,11 @@ using UnityEngine;
 public class BalloonController : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
+
+    private ParticleSystem popEffect;
     private Rigidbody balloonRb;
+    private Renderer renderer;
+
     public float minSpeed;
     public float maxSpeed;
     public float xSpawnRange;
@@ -12,13 +16,33 @@ public class BalloonController : MonoBehaviour
     void Start()
     {
         balloonRb = GetComponent<Rigidbody>();
+        popEffect = GetComponent<ParticleSystem>();
+        renderer = GetComponent<Renderer>();
+
         transform.position = RandomSpawnPos();
         balloonRb.AddForce(RandomForce(), ForceMode.Impulse);
+        var main = popEffect.main;
+        main.startColor = renderer.material.color;
+    }
+
+    void Update()
+    {
+        
     }
 
     private void OnCollisionEnter(Collision other) 
     {
-        Destroy(gameObject);
+        if(!other.gameObject.CompareTag(gameObject.tag))
+        {
+            DestroyBalloon();
+        }
+    }
+
+    private void DestroyBalloon()
+    {
+        renderer.enabled = false;
+        popEffect.Play();
+        Destroy(gameObject, popEffect.main.duration);
     }
 
     Vector3 RandomSpawnPos()
